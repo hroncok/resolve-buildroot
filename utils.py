8ed78e95652e35c4420f006c7be4c6c0681878b5
Miro Hrončok <miro@hroncok.cz>
2021-09-30 20:11:52 +0200

    Utils docs

--- utils.py
+++ utils.py
@@ -2,14 +2,24 @@ import sys
 
 
 def log(*args, **kwargs):
+    """
+    A print replacement that prints to stderr.
+    """
     kwargs.setdefault('file', sys.stderr)
     return print(*args, **kwargs)
 
 
 def name_or_str(thing):
+    """
+    Useful helper to convert various Hawkey/DNF objects to strings.
+    Returns the object's name attribute, falls back to the str representation.
+    """
     return getattr(thing, 'name', str(thing))
 
 
-def stringify(lst, sep=None):
-    sep = sep or ', '
-    return sep.join(name_or_str(i) for i in lst)
+def stringify(lst, separator=', '):
+    """
+    Converts a list of objects to a single string, using the name_or_str() function.
+    If no separator is given, separates the items by comma and space.
+    """
+    return separator.join(name_or_str(i) for i in lst)
