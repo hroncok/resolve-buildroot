3ab76966de170feac9d9f2bbfcd2d0dd712c5db8
Miro Hrončok <miro@hroncok.cz>
2022-06-13 14:42:40 +0200

    Log failures

--- build.py
+++ build.py
@@ -16,52 +16,57 @@ AUTHOR = 'Python Maint <python-maint@redhat.com>'
 TARGET = 'f37-python'
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.exit('This requires one argument.')
-    component_name = sys.argv[1]
+    try:
+        if len(sys.argv) != 2:
+            sys.exit('This requires one argument.')
+        component_name = sys.argv[1]
 
-    bootstrap = None
-    if ':' in component_name:
-        build_reverse_id_lookup()
-        bootstrap = reverse_id_lookup[component_name]
-        component_name, *_ = component_name.partition(':')
+        bootstrap = None
+        if ':' in component_name:
+            build_reverse_id_lookup()
+            bootstrap = reverse_id_lookup[component_name]
+            component_name, *_ = component_name.partition(':')
 
-    # XXX make a reusable function with just refresh_gitrepo/clone_into
-    repopath = FEDPKG_CACHEDIR / component_name
-    if repopath.exists():
-        refresh_gitrepo(repopath, prune_exisitng=True)
-    else:
-        FEDPKG_CACHEDIR.mkdir(exist_ok=True)
-        clone_into(component_name, repopath)
+        # XXX make a reusable function with just refresh_gitrepo/clone_into
+        repopath = FEDPKG_CACHEDIR / component_name
+        if repopath.exists():
+            refresh_gitrepo(repopath, prune_exisitng=True)
+        else:
+            FEDPKG_CACHEDIR.mkdir(exist_ok=True)
+            clone_into(component_name, repopath)
 
-    specpath = repopath / f'{component_name}.spec'
+        specpath = repopath / f'{component_name}.spec'
 
-    # Find any patches from previous bootstrap builds
-    patch = PATCHDIR / f'{component_name}.patch'
-    if patch.exists():
+        # Find any patches from previous bootstrap builds
+        patch = PATCHDIR / f'{component_name}.patch'
+        if patch.exists():
+            if bootstrap:
+                raise NotImplementedError('Double bootstrap is not yet supported')
+            with patch.open('r') as patchfile:
+                run('patch', '-R', '-p1', stdin=patchfile, cwd=repopath)
+            patch.unlink()
+
         if bootstrap:
-            raise NotImplementedError('Double bootstrap is not yet supported')
-        with patch.open('r') as patchfile:
-            run('patch', '-R', '-p1', stdin=patchfile, cwd=repopath)
-        patch.unlink()
+            message = BOOTSTRAP_MESSAGE
+            patch_spec(specpath, bootstrap)
+            diff = run('git', '-C', repopath, 'diff').stdout
+            patch.write_text(diff)
+        else:
+            message = REBUILT_MESSAGE
 
-    if bootstrap:
-        message = BOOTSTRAP_MESSAGE
-        patch_spec(specpath, bootstrap)
-        diff = run('git', '-C', repopath, 'diff').stdout
-        patch.write_text(diff)
-    else:
-        message = REBUILT_MESSAGE
+        # Bump and commit only if we haven't already, XXX ability to force this
+        head_commit_msg = run('git', '-C', repopath, 'log', '--format=%B', '-n1', 'HEAD').stdout.rstrip()
+        if bootstrap or head_commit_msg != message:
+            run('rpmdev-bumpspec', '-c', message, '--userstring', AUTHOR, specpath)
+            run('git', '-C', repopath, 'commit', f'{component_name}.spec', '-m', message, '--author', AUTHOR)
 
-    # Bump and commit only if we haven't already, XXX ability to force this
-    head_commit_msg = run('git', '-C', repopath, 'log', '--format=%B', '-n1', 'HEAD').stdout.rstrip()
-    if bootstrap or head_commit_msg != message:
-        run('rpmdev-bumpspec', '-c', message, '--userstring', AUTHOR, specpath)
-        run('git', '-C', repopath, 'commit', f'{component_name}.spec', '-m', message, '--author', AUTHOR)
+        #raise NotImplementedError('no pushing yet')
+        run('git', '-C', repopath, 'push')
+        cp = run('fedpkg', 'build', '--fail-fast', '--nowait', '--background', '--target', TARGET, cwd=repopath)
+        print(cp.stdout, file=sys.stderr)
+        # XXX prune this directory becasue we don't want no thousands clones?
+        # maybe we are not gonna need this?
+    except Exception:
+        print(sys.argv[1])
+        sys.exit(1)
 
-    #raise NotImplementedError('no pushing yet')
-    run('git', '-C', repopath, 'push')
-    cp = run('fedpkg', 'build', '--fail-fast', '--nowait', '--background', '--target', TARGET, cwd=repopath)
-    print(cp.stdout)
-    # XXX prune this directory becasue we don't want no thousands clones?
-    # maybe we are not gonna need this?
