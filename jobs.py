6e2c0fd49ff0b27a8b0867b9a4cc140ebd1abd02
Miro Hrončok <miro@hroncok.cz>
2022-06-14 17:36:08 +0200

    Don't make newer rawhide builds remove packages

--- jobs.py
+++ jobs.py
@@ -145,7 +145,7 @@ def are_all_done(*, packages_to_check, all_components, components_done, blocker_
                 # For Copr rebuilds, the Copr EVR must be >= Fedora EVR
                 # For koji rebuilds, this will be always true anyway
                 if done_package.name == required_package.name:
-                    if not done_package.evr_lt(required_package):
+                    if True:  # not done_package.evr_lt(required_package): XXX we only wan this check in copr
                         log(f'      ✔ {required_package.name}')
                         break
                     else:
